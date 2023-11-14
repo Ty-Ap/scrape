2536be2ff852e0237aee2718e1d158ba94b787bd
Ty Aponte <aaustinsk8@aol.com>
2023-11-14 12:53:45 -0800

    commenting for newer folks

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::fs::File;
 use std::io::{Write, BufWriter};
 
 async fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
+    // '?' = shorthand for Result enum
     let res = reqwest::get(url).await?;
     let body = res.text().await?;
-    Ok(body)
+    Ok(body) // no trailing semi-colons bc Rust is an expression oriented language
 }
 
 async fn scrape(url: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -15,7 +16,7 @@ async fn scrape(url: &str) -> Result<(), Box<dyn std::error::Error>> {
     let document = Html::parse_document(&html);
     let selector = Selector::parse("body").unwrap();
 
-    let re = Regex::new(r"\b(f)(r)(e)(e)\b|\b(p)(a)(l)(e)(s)(t)(i)(n)(e)\b")?;
+    let re = Regex::new(r"\b(f)(r)(e)(e)\b|\b(p)(a)(l)(e)(s)(t)(i)(n)(e)\b")?; //this finds pattern matches to scrape
     let mut results = Vec::new();
 
     for element in document.select(&selector) {
@@ -35,7 +36,8 @@ async fn scrape(url: &str) -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() {
-    let url = "https://en.wikipedia.org/wiki/State_of_Palestine";
+    let url = "https://en.wikipedia.org/wiki/State_of_Palestine"; //this is the target URL
+    // match control flow to run the show
     match scrape(url).await {
         Ok(_) => println!("Success!!"),
         Err(e) => println!("Error , {}", e),
